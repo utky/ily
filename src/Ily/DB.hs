398ccb4138ebef534929da2c768a61e9562f56ef
utky <ilyaletre@gmail.com>
2014-09-30 08:30:17 +0900

    changed hdbc to persistent and checked creating schema.

--- src/Ily/DB.hs
+++ src/Ily/DB.hs
@@ -1,98 +1,43 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 module Ily.DB 
-    ( Query
-    , statement
-    , executeStatement
-    , listStatement
-    , raw
-    , action
-    , dbcommit
-    , runDataSource
-    , initializeSchema
-    , cleanSchema
+    ( createSchema
     ) where
 
-import Database.HDBC
-import Database.HDBC.Sqlite3
+import           Database.Persist
+import           Database.Persist.Sqlite
+import           Database.Persist.TH
 
-import System.FilePath
-import Paths_ily
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Project
+    name String
+    description String Maybe 
+    deriving Show
+Release
+    name String
+    description String Maybe
+    project_id ProjectId
+    deriving Show
+Issue
+    title String
+    description String Maybe
+    release_id ReleaseId
+    deriving Show
+Record
+    comments String Maybe
+    started_on Int
+    finished_on Int
+    issue_id IssueId
+    deriving Show
+|]
 
-import Control.Exception as E
-
-type Row = [(String, SqlValue)]
-
-newtype Query e = MkQuery { runQuery :: Connection -> IO e }
-
-instance Monad Query where
-        return e = MkQuery $ \_ -> return e
-        ma >>= f = MkQuery $ \c -> do
-            next <- ioresult c
-            (runQuery $ f next) c
-            where
-                ioresult c = runQuery ma c
-
--- class Entity e where
---     newEntity :: e -> Query e
---     deleteEntity :: e => e -> Query e
-
-
-statement :: String -> Query Statement
-statement s = MkQuery $ \c -> prepare c s
-
-
-executeStatement :: Statement -> [SqlValue] -> Query Integer
-executeStatement s vs = MkQuery $ \_ -> execute s vs
-
-listStatement :: Statement -> [SqlValue] -> Query [Row]
-listStatement s vs = MkQuery $ \_ -> do
-        _ <- execute s vs
-        fetchAllRowsAL s
-
-{- | Connect to sqlite database with specified path and run quey
--}
-runDataSource :: String -> Query a -> IO a
-runDataSource datapath query = bracket
-        (connectSqlite3 datapath)
-        (disconnect)
-        (handler)
-        where
-            handler = runQuery query
-
-dbcommit :: Query ()
-dbcommit = action $ \c -> do
-    commit c
-    return ()
-
-{- | Wrapper for HDBC interfaces
--}
-action :: (Connection -> IO a) -> Query a
-action f = MkQuery f
-
-{- | Execution raw query statement
--}
-raw :: String -> Query ()
-raw s = MkQuery $ \c -> runRaw c s
-        
-{- | Execute DDL
--}
-initializeSchema :: Query ()
-initializeSchema = MkQuery $ \c -> do
-        schema <- schemaDefinition
-        runRaw c schema
-        return ()
-
-{- | Destroy existing schema without backup
--}
-cleanSchema :: Query ()
-cleanSchema = raw $ qBody
-        where
-            tables = ["projects", "releases", "issues", "records"]
-            tablename t = "DROP TABLE " ++ t ++ " ;"
-            qBody = foldr (++) "" $ map tablename tables
-
-{- | Read Schema definition from data file.
--}
-schemaDefinition :: IO String
-schemaDefinition = do
-        sfile <- getDataFileName $ "sql" </> "schema.sql"
-        readFile sfile
+createSchema :: IO ()
+createSchema = runSqlite ":memory:" $ do
+        runMigration migrateAll
--- test/Ily/DBSpec.hs
+++ test/Ily/DBSpec.hs
@@ -3,57 +3,12 @@ module Ily.DBSpec
         ) where
 
 import Test.Hspec
--- import Test.QuickCheck
--- import Control.Exception (evaluate, bracket)
 
-import Data.List (sort)
-import Database.HDBC (getTables, toSql)
--- import Database.HDBC.Sqlite3
-
-
-
-import Ily.DB
-
-type TableName = String
+import Ily.DB (createSchema)
 
 spec :: Spec
 spec = do
     describe "DB" $ do
         it "can create tables" $
-            datasource runGetTables >>= \ts -> 
-                tablenames ts `shouldBe` sort ["projects", "releases", "issues", "records"]
-        it "can clean up tables" $
-            datasource cleanTables >>= \ts ->
-                tablenames ts `shouldBe` []
-        it "can insert and select data" $
-            datasource insert >>= \cnt ->
-                cnt `shouldBe` 1
-        where
-            datasource = runDataSource ":memory:"
-            tablenames = sort . filter (\n -> n /= "sqlite_sequence")
-
-
-runGetTables :: Query [TableName]
-runGetTables = do
-        initializeSchema
-        action getTables
-
-cleanTables :: Query [TableName]
-cleanTables = do
-        initializeSchema
-        cleanSchema
-        action getTables
-
-insert :: Query Integer
-insert = do
-        initializeSchema
-        newProject <- statement "INSERT INTO projects(name, description) VALUES (?, ?)"
-        cnt <- executeStatement newProject [toSql "hello", toSql "world"]
-        dbcommit
-        return cnt
-        -- projectsRow <- statement "SELECT * FROM projects"
-        -- rows <- listStatement projectsRow []
-        -- return $ length rows
-
-
+            createSchema `shouldReturn` ()
 
