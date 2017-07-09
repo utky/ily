3416475ad9f7a2f3553be7b010c291d23b09aea5
utky <ilyaletre@gmail.com>
2017-07-09 12:36:04 +0900

    scanning identifier

--- src/Ily/Lexer.x
+++ src/Ily/Lexer.x
@@ -24,8 +24,13 @@ $hexit       = [0-9 A-F a-f]
 $nl          = [\n\r]
 $large       = [A-Z \xc0-\xd6 \xd8-xde]
 $small       = [a-z \xdf-\xf6 \xf8-xff]
-$alpha       = [a-zA-Z]
-$graphic     = [$small $large $digit]
+$alpha       = [$small $large]
+$unichar     = \x00C0-\xFFFF
+$letter      = [$alpha $unichar]
+-- TODO: accept more sequence
+$graphic     = [$letter $digit]
+
+$symbol = [\!\%\&\$\#\+\-\/\:\<\=\>\?\@\\\~\‘\^\|\*]
 
 $cntrl   = [$large \@\[\\\]\^\_]
 @ascii   = \^ $cntrl | NUL | SOH | STX | ETX | EOT | ENQ | ACK
@@ -37,13 +42,80 @@ $charesc = [abfnrtv\\\"\'\&]
 @gap     = \\ $white+ \\
 @string  = $graphic # [\"\\] | " " | @escape | @gap | $nl
 
+--- Reserved
+
+@reservedOp = "(" | ")" | "[" | "]" | "{" | "}" | "," | ":" | ";" | "..." 
+            | "_" | "=" | "=>" | "->" | "#"
+
+-- Identifies
+
+$symbol = [\!\%\&\$\#\+\-\/\:\<\=\>\?\@\\\~\'\^\|\*]
+$idchar      = [$letter $digit \_\']
+
+@id        = ($letter $idchar*) | $symbol+
+@strid     = $letter $idchar*
+@longid    = (@strid \.)+ @id
+
 sml :-
 
 <0>  $white+               ;
+
+-- Reserved
+<0>  "("                     { action $ const TLParen }
+<0>  ")"                     { action $ const TRParen }
+<0>  "["                     { action $ const TLBracket }
+<0>  "]"                     { action $ const TRBracket }
+<0>  "{"                     { action $ const TLBrace }
+<0>  "}"                     { action $ const TRBrace }
+<0>  ","                     { action $ const TComma }
+<0>  ":"                     { action $ const TColon }
+<0>  ";"                     { action $ const TSemiColon }
+<0>  "..."                   { action $ const TRowWild }
+<0>  "_"                     { action $ const TValWild }
+<0>  "="                     { action $ const TEq }
+<0>  "=>"                    { action $ const TFatArrow }
+<0>  "->"                    { action $ const TArrow }
+<0>  "#"                     { action $ const TSharp }
+<0>  "abstype"               { action $ const TAbsType }
+<0>  "and"                   { action $ const TAnd }
+<0>  "andalso"               { action $ const TAndAlso }
+<0>  "as"                    { action $ const TAs }
+<0>  "case"                  { action $ const TCase }
+<0>  "datatype"              { action $ const TDataType }
+<0>  "do"                    { action $ const TDo }
+<0>  "else"                  { action $ const TElse }
+<0>  "end"                   { action $ const TEnd }
+<0>  "exception"             { action $ const TException }
+<0>  "fn"                    { action $ const TFn }
+<0>  "fun"                   { action $ const TFun }
+<0>  "handle"                { action $ const THandle }
+<0>  "if"                    { action $ const TIf }
+<0>  "in"                    { action $ const TIn }
+<0>  "infix"                 { action $ const TInfix }
+<0>  "infixr"                { action $ const TInfixr }
+<0>  "let"                   { action $ const TLet }
+<0>  "local"                 { action $ const TLocal }
+<0>  "nonfix"                { action $ const TNonfix }
+<0>  "of"                    { action $ const TOf }
+<0>  "op"                    { action $ const TOp }
+<0>  "open"                  { action $ const TOpen } 
+<0>  "orelse"                { action $ const TOrElse }
+<0>  "raise"                 { action $ const TRaise} 
+<0>  "rec"                   { action $ const TRec }
+<0>  "then"                  { action $ const TThen }
+<0>  "type"                  { action $ const TType }
+<0>  "val"                   { action $ const TVal }
+<0>  "with"                  { action $ const TWith }
+<0>  "withtype"              { action $ const TWithType }
+<0>  "while"                 { action $ const TWhile }
+
 <0>  $digit+               { action (TInt . read) }
 <0>  \"                    { begin string }
 <string> @string*          { action TStr }
 <string> \"                { begin 0 }
+
+<0>  @id                  { action TId }
+<0>  @longid                  { action TLongId }
 
 {
 
@@ -51,6 +123,107 @@ sml :-
 data Token
   = TInt Integer
   | TStr String
+  -- abstype
+  | TAbsType
+  -- and
+  | TAnd
+  -- andalso
+  | TAndAlso
+  -- as
+  | TAs
+  -- case
+  | TCase
+  -- datatype
+  | TDataType
+  -- do
+  | TDo
+  -- else
+  | TElse
+  -- end
+  | TEnd
+  -- exception
+  | TException
+  -- fn
+  | TFn
+  -- fun
+  | TFun
+  -- handle
+  | THandle
+  -- if
+  | TIf
+  -- in
+  | TIn
+  -- infix
+  | TInfix
+  -- infixr
+  | TInfixr
+  -- let
+  | TLet
+  -- local
+  | TLocal
+  -- nonfix
+  | TNonfix
+  -- of
+  | TOf
+  -- op
+  | TOp
+  -- open
+  | TOpen
+  -- orelse
+  | TOrElse
+  -- raise
+  | TRaise
+  -- rec
+  | TRec
+  -- then
+  | TThen
+  -- type
+  | TType
+  -- val
+  | TVal
+  -- with
+  | TWith
+  -- withtype
+  | TWithType
+  -- while
+  | TWhile
+  -- (
+  | TLParen
+  -- )
+  | TRParen
+  -- [
+  | TLBracket
+  -- ]
+  | TRBracket
+  -- {
+  | TLBrace
+  -- }
+  | TRBrace
+  -- ,
+  | TComma
+  -- :
+  | TColon
+  -- ;
+  | TSemiColon
+  -- ...
+  | TRowWild
+  -- _
+  | TValWild
+  -- |
+  | TBar
+  -- =
+  | TEq
+  -- ->
+  | TArrow
+  -- =>
+  | TFatArrow
+  -- #
+  | TSharp
+  -- Identifier
+  | TId String
+  -- Long identifier
+  | TLongId String
+  -- End of file
   | TEOF
   deriving (Eq,Show)
 
--- test/Ily/LexerSpec.hs
+++ test/Ily/LexerSpec.hs
@@ -7,19 +7,154 @@ import qualified Ily.Lexer as L
 shouldScan :: String -> L.Token -> Expectation
 shouldScan s t = L.run L.scanner s `shouldBe` (Right t)
 
+shouldFail :: String -> Expectation
+shouldFail s = L.run L.scanner s `shouldSatisfy` isFailure where
+  isFailure (Right _) = False
+  isFailure (Left _) = True
+
 spec :: Spec
 spec = do
 
-  it "scans 1 digit integer" $ 
-    "1" `shouldScan` (L.TInt 1)
-  it "scans 2 digit integer" $
-    "12" `shouldScan` (L.TInt 12)
-  it "scans negative integer" $
-    "-1" `shouldScan` (L.TInt (-1))
+  describe "integer" $ do
+    it "scans 1 digit integer" $ 
+      "1" `shouldScan` (L.TInt 1)
+    it "scans 2 digit integer" $
+      "12" `shouldScan` (L.TInt 12)
+    it "scans negative integer" $
+      "-1" `shouldScan` (L.TInt (-1))
 
-  it "scans alphabet string" $
-    "\"a\"" `shouldScan` (L.TStr "a")
-  it "scans alphabet with newline string" $
-    "\"a\nb\"" `shouldScan` (L.TStr "a\nb")
-  it "scans alphabet with carrige return string" $
-    "\"a\r\nb\"" `shouldScan` (L.TStr "a\r\nb")
+  describe "string" $ do
+    it "scans alphabet string" $
+      "\"a\"" `shouldScan` (L.TStr "a")
+    it "scans alphabet with newline string" $
+      "\"a\nb\"" `shouldScan` (L.TStr "a\nb")
+    it "scans alphabet with carrige return string" $
+      "\"a\r\nb\"" `shouldScan` (L.TStr "a\r\nb")
+    it "scans multibyte string" $
+      "\"文字列\"" `shouldScan` (L.TStr "文字列")
+    it "scans multibyte with newline string" $
+      "\"文字列\nalpha123\"" `shouldScan` (L.TStr "文字列\nalpha123")
+
+  describe "reserved" $ do
+
+    it "scans (" $
+      "(" `shouldScan` L.TLParen
+    it "scans )" $
+      ")" `shouldScan` L.TRParen
+    it "scans [" $
+      "[" `shouldScan` L.TLBracket
+    it "scans ]" $
+      "]" `shouldScan` L.TRBracket
+    it "scans {" $
+      "{" `shouldScan` L.TLBrace
+    it "scans }" $
+      "}" `shouldScan` L.TRBrace
+    it "scans ," $
+      "," `shouldScan` L.TComma
+    it "scans :" $
+      ":" `shouldScan` L.TColon
+    it "scans ;" $
+      ";" `shouldScan` L.TSemiColon
+    it "scans ..." $
+      "..." `shouldScan` L.TRowWild
+    it "scans _" $
+      "_" `shouldScan` L.TValWild
+    it "scans =" $
+      "=" `shouldScan` L.TEq
+    it "scans =>" $
+      "=>" `shouldScan` L.TFatArrow
+    it "scans ->" $
+      "->" `shouldScan` L.TArrow
+    it "scans #" $
+      "#" `shouldScan` L.TSharp
+
+    it "scans abstype" $
+      "abstype" `shouldScan` L.TAbsType
+    it "scans and" $
+      "and" `shouldScan` L.TAnd
+    it "scans andalso" $
+      "andalso" `shouldScan` L.TAndAlso
+    it "scans as" $
+      "as" `shouldScan` L.TAs
+    it "scans case" $
+      "case" `shouldScan` L.TCase
+    it "scans datatype" $
+      "datatype" `shouldScan` L.TDataType
+    it "scans do" $
+      "do" `shouldScan` L.TDo
+    it "scans else" $
+      "else" `shouldScan` L.TElse
+    it "scans end" $
+      "end" `shouldScan` L.TEnd
+    it "scans exception" $
+      "exception" `shouldScan` L.TException
+    it "scans fn" $
+      "fn" `shouldScan` L.TFn
+    it "scans fun" $
+      "fun" `shouldScan` L.TFun
+    it "scans handle" $
+      "handle" `shouldScan` L.THandle
+    it "scans if" $
+      "if" `shouldScan` L.TIf
+    it "scans in" $
+      "in" `shouldScan` L.TIn
+    it "scans infix" $
+      "infix" `shouldScan` L.TInfix
+    it "scans infixr" $
+      "infixr" `shouldScan` L.TInfixr
+    it "scans let" $
+      "let" `shouldScan` L.TLet
+    it "scans local" $
+      "local" `shouldScan` L.TLocal
+    it "scans nonfix" $
+      "nonfix" `shouldScan` L.TNonfix
+    it "scans of" $
+      "of" `shouldScan` L.TOf
+    it "scans op" $
+      "op" `shouldScan` L.TOp
+    it "scans open" $
+      "open" `shouldScan` L.TOpen
+    it "scans orelse" $
+      "orelse" `shouldScan` L.TOrElse
+    it "scans raise" $
+      "raise" `shouldScan` L.TRaise
+    it "scans rec" $
+      "rec" `shouldScan` L.TRec
+    it "scans then" $
+      "then" `shouldScan` L.TThen
+    it "scans type" $
+      "type" `shouldScan` L.TType
+    it "scans val" $
+      "val" `shouldScan` L.TVal
+    it "scans with" $
+      "with" `shouldScan` L.TWith
+    it "scans withtype" $
+      "withtype" `shouldScan` L.TWithType
+    it "scans while" $
+      "while" `shouldScan` L.TWhile
+
+  describe "identifier" $ do
+    it "scans alphabet identifier" $
+      "a" `shouldScan` (L.TId "a")
+    it "scans alphabet and digit identifier" $
+      "a1" `shouldScan` (L.TId "a1")
+    it "scans alphabet and underscore and digit identifier" $
+      "a_1" `shouldScan` (L.TId "a_1")
+    it "scans alphabet and prime identifier" $
+      "a'" `shouldScan` (L.TId "a'")
+    it "scans multibyte identifier" $
+      "識別子" `shouldScan` (L.TId "識別子")
+    it "scans mixed identifier" $
+      "識別子_a1" `shouldScan` (L.TId "識別子_a1")
+    it "doesn't scan digit headed identifier" $
+      shouldFail "1識別子"
+    it "scans primed reserved identifier" $
+      "type'" `shouldScan` (L.TId "type'")
+    it "scans && identifier" $
+      "&&" `shouldScan` (L.TId "&&")
+    it "scans || identifier" $
+      "||" `shouldScan` (L.TId "||")
+    it "scans single heading long identifier" $
+      "foo.bar" `shouldScan` (L.TLongId "foo.bar")
+    it "scans double heading long identifier" $
+      "foo.bar.qux" `shouldScan` (L.TLongId "foo.bar.qux")
